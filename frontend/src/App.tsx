930ee6dc354a51fa8b32691cebacf0e8e51b58b1
Hamnath <54175484+Hamnath-Hamnath@users.noreply.github.com>
2023-02-21 21:23:54 +0530

    Front Completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import MainLayout from './Layouts/MainLayout';
+import HomeScreen from './screens/HomeScreen';
+import {Routes,Route} from 'react-router-dom'
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <MainLayout>
+      <Routes>
+        <Route element={<HomeScreen/>} path='/' />
+        <Route element={<ProductScreen/>} path='/product/:id' />
+      </Routes>
+    </MainLayout>
   );
 }
 
